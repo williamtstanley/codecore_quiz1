db9a69ef40af30908fbe963f2734c385e3bb3182
William Stanley <william.t.stanley@gmail.com>
2016-05-30 11:50:29 -0700

    added question 11 - Already written that way?

---
+++ q11_titleize.rb
@@ -0,0 +1,13 @@
+# Implement the following code in Ruby: Create a module called HelperMethods that include a method called `titleize` that does the following: it takes in a string and returns the string back after capitalizing each word in that string. For example if you pass to the method a string "hello world" you should get back "Hello World". The methods should not capitalize the following words: in, the, of, and, or, from.
+
+
+
+module HelperMethods
+
+    def titleize(string)
+        words = string.split
+        (words.map {|word| words.index(word) != 0 && (word == "in" || word == "the" ||  word == "of" || word == "and" || word == "or" || word == "from" || word == "is") ? word : word.capitalize}).join(" ")
+    end
+
+end
+
